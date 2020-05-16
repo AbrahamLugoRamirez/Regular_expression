2c739b0940b87ad8611acab74f54066026c1e139
Abraham Lugo Ramirez <48779532+AbrahamLugoRamirez@users.noreply.github.com>
2020-05-16 18:21:33 -0500

    No reconoce \ se tiene que poner el /

--- rutaDOS.pl
+++ rutaDOS.pl
@@ -1,10 +1,24 @@
 #!/usr/bin/perl
-my $texto = "F:";
-$letra = "a-z|A-Z";
-$variable = "a-z|0-9";
+#Abraham Lugo Ramirez
+#Cristian Zapata
+#Leider Pineda
+#Nino Mercado
 
-if($texto =~ /^[$letra][:]$/) {
-  print "entro";
+#Definiciones regulares
+my $texto = "C:/Program";
+$digito = "0-9";
+$letra = "a-zA-z";
+$char = "$digito|$letra| ";
+
+#$chars = "[$char]*";
+$ext = "[\.][$char][$char]?[$char]?";
+$nombre = "[$char]+";
+$archivo = "$nombre$ext"; 
+#[\.][$char][$char]?[$char]?
+
+#validacion
+if($texto =~ /^([$letra][:])?([\/][$char]+($ext)?)*$/) {
+  print "Correcto";
 }else{
-    print "noo";
+    print "incorrecto";
 }
