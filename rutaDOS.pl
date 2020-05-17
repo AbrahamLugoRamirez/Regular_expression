0cf467c9f051e457e7d0834b2874efd3e2dda697
Abraham Lugo Ramirez <labrahamdedios@gmail.com>
2020-05-17 14:36:15 -0500

    ya acepta \

--- rutaDOS.pl
+++ rutaDOS.pl
@@ -1,21 +1,17 @@
 #!/usr/bin/perl
-#Abraham Lugo Ramirez
-#Cristian Zapata
-#Leider Pineda
-#Nino Mercado
-
 #Definiciones regulares
-my $texto = "C:/Program";
+#my $texto = "C:/Program";
+print "Digite la direccion DOS a verificar: ";
+my $texto=<STDIN>;
 $digito = "0-9";
 $letra = "a-zA-z";
-$char = "$digito|$letra| ";
+$char = "$digito|$letra|";
 $ext = "[\.][$char][$char]?[$char]?";
 $nombre = "[$char]+";
 $archivo = "$nombre$ext"; 
 
-
 #validacion
-if($texto =~ /^([$letra][:])?([\/][$char]+($ext)?)*$/) {
+if($texto =~ /^([$letra][:])?([\\][$char]+($ext)?)*$/) {
   print "Direccion correcta";
 }else{
     print "Direccion incorrecta";
