a6912be613c110bba7e771cca6a504e152ef8a1d
Abraham Lugo Ramirez <labrahamdedios@gmail.com>
2020-05-22 13:30:19 -0500

    VerificarIP

--- verificarIP.pl
+++ verificarIP.pl
@@ -6,6 +6,7 @@ $digito = "0-9";
 $digcinco = "0-5";
 $digcuatro = "0-4";
 $vali = "(?:25[$digcinco]|2[$digcuatro][$digito]|[01]?[$digito][$digito]?)";
+
 #validacion
 if($texto =~ /^(?:$vali\.){3}$vali$/) {
   print "Direccion IP correcta";
