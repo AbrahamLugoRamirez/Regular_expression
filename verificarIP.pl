c931acb32c98a0c85d0f2e8c58e357d84edb9831
Abraham Lugo Ramirez <labrahamdedios@gmail.com>
2020-05-26 16:12:38 -0500

    Ip definitions

--- verificarIP.pl
+++ verificarIP.pl
@@ -1,14 +1,14 @@
 #!/usr/bin/perl
-
 print "Digite la IP a verificar: ";
 my $texto=<STDIN>;
-
 #Definiciones regulares
 $digito = "0-9";
 $digcinco = "0-5";
 $digcuatro = "0-4";
-$vali = "(?:25[$digcinco]|2[$digcuatro][$digito]|[01]?[$digito][$digito]?)";
-
+$caso1 = "25[$digcinco]";
+$caso2 = "2[$digcuatro][$digito]";
+$caso3 = "[01]?[$digito][$digito]?";
+$vali = "(?:$caso1|$caso2|$caso3)";
 #validacion
 if($texto =~ /^(?:$vali\.){3}$vali$/) {
   print "Direccion IP correcta";
