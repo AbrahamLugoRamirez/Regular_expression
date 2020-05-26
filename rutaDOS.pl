f98a850c9ded76e61bee70d1466eb28af08cd2e5
Nino Jesus Mercado Consuegra <njmercado@uninorte.edu.co>
2020-05-25 22:03:15 -0500

    Varibles usadas para formar el condicional y no tener que reescribir toda la expresión de cero

--- rutaDOS.pl
+++ rutaDOS.pl
@@ -1,18 +1,34 @@
 #!/usr/bin/perl
-#Definiciones regulares
-#my $texto = "C:/Program";
-print "Digite la direccion DOS a verificar: ";
-my $texto=<STDIN>;
+# Definiciones regulares
+my $texto = "C:/Program";
+ print "Digite la direccion DOS a verificar: ";
+ my $texto=<STDIN>;
 $digito = "0-9";
 $letra = "a-zA-z";
 $char = "$digito|$letra|";
 $ext = "[\.][$char][$char]?[$char]?";
-$nombre = "[$char]+";
-$archivo = "$nombre$ext"; 
+$nombre = "[$char|\s]+";
+$archivo = "$nombre($ext)?"; 
 
 #validacion
-if($texto =~ /^([$letra][:])?([\\][$char|\s]+($ext)?)*$/) {
+if($texto =~ /^([$letra][:])?([\\]$archivo)*$/) {
   print "Direccion correcta";
 }else{
     print "Direccion incorrecta";
 }
+
+#print "Digite la direccion DOS a verificar: ";
+#my $texto=<STDIN>;
+##Definiciones regulares
+#$digito = "0-9";
+#$letra = "a-zA-z";
+#$char = "$digito|$letra|";
+#$ext = "[\.][$char][$char]?[$char]?";
+#$nombre = "[$char|\s]+";
+#$archivo = "$nombre[$ext]?"; 
+##validacion
+#if($texto =~ /^([$letra][:])?([\\][$archivo])+$/) {
+#  print "Direccion DOS correcta";
+#}else{  
+#  print "Dirección DOS incorrecta";
+#}
